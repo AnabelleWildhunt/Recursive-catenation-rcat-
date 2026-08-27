1e9eaeeca286a74bcae456fca352dac5a664a106
AnabelleWildhunt <mak.noemi5@gmail.com>
2026-08-27 14:49:24 +0200

    Added the if statement, checking wheather the file is a directory and if so, executing the script within said directory, thus making the promised recursiveness. Also, added the line which informs which prints the currently concatinated file path in distinguishable color

--- rcat.sh
+++ rcat.sh
@@ -7,6 +7,14 @@ list=$(ls -l $pwd/$1 | awk 'NR > 1 {print $NF}')
 
 for i in $list
 do
-	cat $pwd/$1/$i
-	echo
+	if [[ -d "$pwd/$1/$i" ]]
+	then
+		"$0" "$1/$i"
+	else
+		echo
+		printf '\e[33m%s\e[0m\n' "$pwd/$1/$i"
+		echo
+		cat $pwd/$1/$i
+		echo
+	fi
 done
